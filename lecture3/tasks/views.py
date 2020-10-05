bdbe67050c9646ce6bd66555db9f730cffce1072
thenatural86 <philip.smalls86@gmail.com>
2020-10-05 17:20:32 -0400

    redirect

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,8 +1,19 @@
 from django import forms
 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+tasks = []
+
+# new class to represent form
+class NewTaskForm(forms.Form):
+    # define all the fields i want the form to have
+    task = forms.CharField(label="New Task")
+    # client side validation
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+
+
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html", {
@@ -10,4 +21,21 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    # server side validation
+    if request.method == "POST":
+        # fills in data from form
+        form = NewTaskForm(request.POST)
+        # validation function
+        if form.is_valid():
+           task = form.cleaned_data["task"]
+           tasks.append(task)
+           return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    # newtaskform passed in as context key/val
+    # GET
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
