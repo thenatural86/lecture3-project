9774e1f281dcde608273b0831cf7335fb9c85fb9
thenatural86 <philip.smalls86@gmail.com>
2020-10-05 10:29:14 -0400

    render greet

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -13,4 +13,6 @@ def david(request):
     return HttpResponse("Hello, David!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
