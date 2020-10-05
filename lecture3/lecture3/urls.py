968716d75c4aef0973a805b871eae2424475d738
thenatural86 <philip.smalls86@gmail.com>
2020-10-05 13:52:27 -0400

    add tasks form

--- lecture3/lecture3/urls.py
+++ lecture3/lecture3/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# table of contents for app
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
+    path('tasks/', include("tasks.urls")),
     path('newyear/', include("newyear.urls"))
 ]
---
+++ lecture3/tasks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    # single empty route that loads the index function
+    path("", views.index, name="index"),
+    path("add", views.add, name="add")
+]
