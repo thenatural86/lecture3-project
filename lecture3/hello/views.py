cc80e39c78649de4d56be33f53d1b44025daf0b7
thenatural86 <philip.smalls86@gmail.com>
2020-10-05 10:13:41 -0400

    templates

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -2,19 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
-
 def index(request):
-    return HttpResponse("Hello, world!")
-
+    # pass in request to render function, along with an html template
+    return render(request, "hello/index.html")
 
 def brian(request):
     return HttpResponse("Hello, brian!")
 
-
 def david(request):
     return HttpResponse("Hello, David!")
-
 
 def greet(request, name):
     return HttpResponse(f"Hello, {name.capitalize()}!")
