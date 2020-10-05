f842c0d7f1bd1ca022013e3e82236cc6086192ca
thenatural86 <philip.smalls86@gmail.com>
2020-10-04 23:23:57 -0400

    routing

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -6,3 +6,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+
+def brian(request):
+    return HttpResponse("Hello, brian!")
+
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
